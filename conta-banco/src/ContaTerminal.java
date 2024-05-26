ed5c9b7ec0bd8abfe471135176922c076f022459
Luis Felipe de Moraes <felipe.luis2415@gmail.com>
2024-05-25 23:43:04 -0300

    Implementacao das funcoes para gerar uma nova conta no banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -2,18 +2,29 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
-
     public static void main(String[] args) throws Exception {
         String agency;
         String clientName;
         Integer numberCount;
-        Float balance;
+        Double balance;
 
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Olá, bem vindo ao nosso banco!\n------------------------------\n");
         System.out.println("Digite o seu nome!");
         clientName = scanner.next();
+
+        System.out.println("Digite o número da sua agencia!");
+        agency = scanner.next();
+
+        System.out.println("Digite o numero da sua conta!");
+        numberCount = scanner.nextInt();
+
+        System.out.println("Para finalizar, o valor depositado!");
+        balance = scanner.nextDouble();
+
+        System.out.println("Olá " + clientName + ", obrigado por criar uma conta em nosso banco, sua agência é "
+            +agency+", conta "+numberCount+" e seu saldo "+balance+" já está disponível para saque");
 
         scanner.close();
     }
